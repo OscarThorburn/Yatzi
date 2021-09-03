8b76e7249f02359e30b92e3825b40276e3b80e19
Oscar Thorburn <oscar.thorburn@iths.se>
2021-09-03 17:25:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yatzi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
